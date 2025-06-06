e825a076527f6781cdb34509ea34c5fad00fc2a6
Gabriel <carmooliveiragabriel@gmail.com>
2025-06-06 14:36:37 -0300

    reparando js

--- js/script.js
+++ js/script.js
@@ -5,13 +5,11 @@ const logoFooter = document.getElementById('logo-footer');
 const temaIcone = temaBtn.querySelector('i'); // pega o <i> dentro do botão
 
 temaBtn.addEventListener('click', () => {
-    // alterna a classe 'light' no body
+
     body.classList.toggle('light');
 
-    // verifica se está no tema claro
     const isLight = body.classList.contains('light');
 
-    // troca os logos conforme o tema
     logo.src = isLight
         ? 'Assets/icon/logo-dark.svg'
         : 'Assets/icon/logo.svg';
@@ -20,7 +18,6 @@ temaBtn.addEventListener('click', () => {
         ? 'Assets/icon/logo-footer-dark.svg'
         : 'Assets/icon/logo-footer.svg';
 
-    // troca o ícone (sol ↔ lua)
     temaIcone.classList.toggle('fa-sun-bright', !isLight);
     temaIcone.classList.toggle('fa-moon', isLight);
     });
