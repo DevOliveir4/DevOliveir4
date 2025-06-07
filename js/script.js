3b52d2f8d7d28fa1be2865eab225bd974a523cb9
Gabriel <carmooliveiragabriel@gmail.com>
2025-06-07 17:22:43 -0300

    implementei a troca dos mockup dependendo da largura da tela

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
-const temaBtn = document.querySelector('.tema');
-const body = document.body;
-const logo = document.getElementById('logo');
-const logoFooter = document.getElementById('logo-footer');
-const temaIcone = temaBtn.querySelector('i'); 
-temaBtn.addEventListener('click', () => {
 
-    body.classList.toggle('light');
+document.addEventListener('DOMContentLoaded', () => {
 
-    const isLight = body.classList.contains('light');
+    const temaBtn = document.querySelector('.tema');
+    const body = document.body;
+    const logo = document.getElementById('logo');
+    const logoFooter = document.getElementById('logo-footer');
+    const temaIcone = temaBtn.querySelector('i'); 
 
-    logo.src = isLight
-        ? 'Assets/icon/logo-dark.svg'
-        : 'Assets/icon/logo.svg';
+  
+    temaBtn.addEventListener('click', () => {
+   
+        body.classList.toggle('light');
 
-    logoFooter.src = isLight
-        ? 'Assets/icon/logo-footer-dark.svg'
-        : 'Assets/icon/logo-footer.svg';
+        const isLight = body.classList.contains('light');
 
-    temaIcone.classList.toggle('fa-sun-bright', !isLight);
-    temaIcone.classList.toggle('fa-moon', isLight);
+        logo.src = isLight
+            ? 'Assets/icon/logo-dark.svg'
+            : 'Assets/icon/logo.svg';
+
+     
+        logoFooter.src = isLight
+            ? 'Assets/icon/logo-footer-dark.svg'
+            : 'Assets/icon/logo-footer.svg';
+
+        temaIcone.classList.toggle('fa-sun-bright', !isLight);
+        temaIcone.classList.toggle('fa-moon', isLight);
     });
+    
+
+    const mockupImages = document.querySelectorAll('.mockup');
+
+    function ajustarImagens() {
+        const larguraTela = window.innerWidth;
+
+        mockupImages.forEach(img => {
+
+            if (!img.dataset.srcOriginal) {
+                img.dataset.srcOriginal = img.src;
+            }
+
+            const srcOriginal = img.dataset.srcOriginal;
+            const srcMobile = srcOriginal.replace('.png', '-inteiro.png');
+
+
+            if (larguraTela < 1128) {
+                img.src = srcMobile;
+            } else {
+                img.src = srcOriginal;
+            }
+        });
+    }
+
+
+    window.addEventListener('resize', ajustarImagens);
+
+    ajustarImagens();
+
+});
